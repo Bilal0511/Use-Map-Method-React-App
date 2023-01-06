fa6cffa2ef94c1ec41ed09bc1ba66f533c573c23
Bilal0511 <bilaltanveer0511@gmail.com>
2023-01-06 19:24:02 +0500

    My first commit

---
+++ hero/Img2.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Img2() {
+  return (
+    <>
+      <div class="carousel-item vh-100">
+          <img src="https://st2.depositphotos.com/1579454/10057/i/600/depositphotos_100579796-stock-photo-machine-code-languages.jpg" class="d-block w-100" alt="head"/>
+        </div>
+    </>
+  );
+}
+
+export default Img2;
